c23f4c9f77424a5579df479b8108b48726d6d9d8
ciaoly <ciaoly@outlook.com>
2021-11-07 18:29:23 +0800

    Now multi tasks enabled.

--- index.js
+++ index.js
@@ -3,26 +3,29 @@ import LuckyDog, { randomNum, sleep } from './LuckyDog.js';
 const proxy = Proxy();
 const port = 8081;
 
-let fuckedFlag = false;
-async function fuckYou(token = "") {
-	if (fuckedFlag) {
+let fuckedFlag = new Map();
+function fuckYou(token = "") {
+	if (token.indexOf("Bearer") < 0) {
+		token = `Bearer ${token}`;
+	}
+	if (fuckedFlag.get(token)) {
 		return;
 	}
-	fuckedFlag = !fuckedFlag;
+	fuckedFlag.set(token, !fuckedFlag.get(token));
 	console.log(`Token是${token}`);
 	console.log("已经获取到Token了, 稍等1分钟左右开始自动答题");
-	await sleep(randomNum(60, 100) * 1000);
-	try {
-		let res = await LuckyDog(token);
-	} catch (e) {
+	sleep(randomNum(60, 100) * 1000).then(res => LuckyDog(token)).then(res => {
+		fuckedFlag.delete(token);
+	}).catch(e => {
 		if (e.code == -9999) {
 			console.log("Token无效, 点一点网页, 程序会自动地再抓一个");
-			fuckedFlag = false;
+			fuckedFlag.delete(token);
 		} else {
 			console.log("出错了");
 			console.error(e);
+			fuckedFlag.set(token, false);
 		}
-	}
+	});
 }
 
 proxy.onError(function (ctx, err) {
@@ -30,18 +33,18 @@ proxy.onError(function (ctx, err) {
 		console.info("遇到一个证书错误, 不过不用管, 因为答题平台是HTTP的, 和这个错误无关");
 		return;
 	}
-	if (!fuckYou) {
+	//if (!fuckYou) {
 		console.info("代理出错了, 不用管它, 只要获取到了Token就行:", err);
-	}
+	//}
 });
 
 proxy.onRequest(function (ctx, callback) {
 	const headers = ctx.clientToProxyRequest.headers;
-	if (headers.host.indexOf(".gsosc.cn") >= 0 && !fuckedFlag) {
+	if (headers.host.indexOf(".gsosc.cn") >= 0) {
 		if (headers.authorizationlg) {
 			fuckYou(headers.authorizationlg);
 		} else if (headers.cookie && headers.cookie.indexOf("WChart-Token") >= 0) {
-			fuckYou(headers.cookie.replace("WChart-Token=", ""))
+			fuckYou(headers.cookie.replace("WChart-Token=", "Bearer "));
 		}
 	}
 	return callback();
